6fb9e829ed0be07cb7f11584b64da34310569e17
salpizar <salpizar@outcoding.com>
2022-03-22 15:50:14 -0600

    Added file with main methods definitions

---
+++ code/dates.py
@@ -0,0 +1,14 @@
+def fecha_es_tupla():
+    print("Hello World")
+
+def bisiesto():
+    print("Hello World")
+
+def fecha_es_vailida():
+    print("Hello World")
+
+def ordinal_dia():
+    print("Hello World")
+
+def imprimir_3x4():
+    print("Hello World")
